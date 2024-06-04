bb7b016bbf2a9c14f4cea3cb5311bbdac3a70fb4
noah <nounaser19@gmail.com>
2024-06-04 18:15:54 +0300

    hello changes v2

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -10,52 +10,52 @@ import { map } from 'rxjs/operators';
 })
 export class AppComponent  {
   loadedPosts = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.fetchPosts();
-  }
-
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
-  }
-
-  onFetchPosts() {
-    // Send Http request
-    this.fetchPosts();
-  }
-
-  onClearPosts() {
-    // Send Http request
-  }
-
-  private fetchPosts() {
-    this.http
-      .get('https://ng-complete-guide-c56d3.firebaseio.com/posts.json')
-      .pipe(
-        map(responseData => {
-          const postsArray = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              postsArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return postsArray;
-        })
-      )
-      .subscribe(posts => {
-        // ...
-        console.log(posts);
-      });
-  }
-
 }
+//   constructor(private http: HttpClient) {}
+
+//   ngOnInit() {
+//     this.fetchPosts();
+//   }
+
+//   onCreatePost(postData: { title: string; content: string }) {
+//     // Send Http request
+//     this.http
+//       .post(
+//         'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+//         postData
+//       )
+//       .subscribe(responseData => {
+//         console.log(responseData);
+//       });
+//   }
+
+//   onFetchPosts() {
+//     // Send Http request
+//     this.fetchPosts();
+//   }
+
+//   onClearPosts() {
+//     // Send Http request
+//   }
+
+//   private fetchPosts() {
+//     this.http
+//       .get('https://ng-complete-guide-c56d3.firebaseio.com/posts.json')
+//       .pipe(
+//         map(responseData => {
+//           const postsArray = [];
+//           for (const key in responseData) {
+//             if (responseData.hasOwnProperty(key)) {
+//               postsArray.push({ ...responseData[key], id: key });
+//             }
+//           }
+//           return postsArray;
+//         })
+//       )
+//       .subscribe(posts => {
+//         // ...
+//         console.log(posts);
+//       });
+//   }
+
+// }
